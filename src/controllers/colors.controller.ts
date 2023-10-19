2004ad2213c76413a82a06b33738fc33f5016fbe
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-10-19 17:41:44 +0300

    add color controller and service

--- src/controllers/colors.controller.ts
+++ src/controllers/colors.controller.ts
@@ -1,1 +1,12 @@
+import { ControllerAction } from '../types';
+import { ColorService } from '../services/colors.service';
 
+const getAll: ControllerAction = (req, res) => {
+  const colors = ColorService.getAll();
+
+  res.send(colors);
+};
+
+export const ColorsController = {
+  getAll,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { UsersController } from './controllers/users.controller';
-// import { ColorsController } from './controllers/colors.controller';
+import { ColorsController } from './controllers/colors.controller';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,16 +15,6 @@ app.use(cors({
   origin: CLIENT_ORIGIN,
 }));
 
-const colors = [
-  { id: 1, name: 'Black' },
-  { id: 2, name: 'DeepPink' },
-  { id: 3, name: 'Red' },
-  { id: 4, name: 'Aquamarine' },
-  { id: 5, name: 'Gold' },
-  { id: 6, name: 'YellowGreen' },
-  { id: 7, name: 'Yellow' },
-];
-
 app.get('/', (req, res) => {
     res.end('Hello world!');
 });
@@ -35,9 +25,7 @@ app.post('/users', express.json(), UsersController.create);
 
 app.get('/users/:userId', UsersController.getById);
 
-app.get('/colors', (req, res) => {
-    res.send(colors);
-});
+app.get('/colors', ColorsController.getAll);
 
 app.listen(PORT, () => {
     console.log(`API is running on http://localhost:${PORT}`);
